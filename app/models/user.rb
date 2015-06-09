b975663747848a0257f10ae22886fbb2a8c72a61
sebasking01 <noordinarypeopleallowed@gmail.com>
2015-06-08 22:29:30 -0400

    added destroy dependeny for appointments and identity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_one :identity
-  has_many :appointments
+  has_one :identity, dependent: :destroy
+  has_many :appointments, dependent: :destroy
 
   def self.create_with_omniauth(auth)
     User.create({provider: auth[:provider], uid: auth[:uid], name: auth[:info][:nickname]})
