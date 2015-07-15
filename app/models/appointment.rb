c7065c91233ddfca8cf289ad38f50564d95e5b2c
Sebasking01 <noordinarypeopleallowed@gmail.com>
2015-07-15 00:31:19 -0400

    edited the appointment model and additional work google calendar api wrapper

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,16 @@
 class Appointment < ActiveRecord::Base
   belongs_to :user
+
+  def add_appointment
+    GoogleCalendar.create_appointment(email, startime, endtime, token)
+
+  end
+
+  def remove_appointment(email, token)
+    GoogleCalendar.cancel_appointment(email, token)
+  end
+
+  def edit_user_appointment(email, token)
+    GoogleCalendar.edit_appointment(email, token)
+  end
 end
